d18e62a4ed22e6d16fb6ca688f827c0bef254009
HarleyMarley88 <xapa.ultras@yandex.ru>
2026-06-16 15:08:10 +0300

    update simulation log config

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -5,18 +5,27 @@ from datetime import datetime
 import json
 import random
 import time
+import logging
 
 producer = KafkaProducer(
     bootstrap_servers="kafka:9092",
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
+logging.basicConfig(
+    filename="/logs/simulation.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    force=True
+)
+
+logger = logging.getLogger("simulation")
 TAG_NAME = "reactor_temp"
 
 while True:
-
+    ts = now.isoformat()
     now = datetime.now()
-
+    day = ts.strftime("%Y-%m-%d")
     value = round(
         random.uniform(70, 80),
         2
@@ -24,17 +33,37 @@ while True:
 
     data = {
         "tag_name": TAG_NAME,
-        "timestamp": now.isoformat(),
+        "timestamp": ts,
         "value": value
     }
+    try:
+        producer.send(
+            "telemetry",
+            data
+        )
 
-    producer.send(
-        "telemetry",
-        data
-    )
+        producer.flush()
 
-    producer.flush()
-
-    print(data)
+        logging.info(
+                json.dumps({
+                    "event": "simulation_input",
+                    "source": "simulation",
+                    "tag_name": TAG_NAME,
+                    "value": value,
+                    "day": day,
+                    "status": "success",
+                    "timestamp": ts
+                })
+            )
+    except Exception as e:
+        
+        logging.error(
+        json.dumps({
+            "event":"simulation_input",
+            "status":"error",
+            "tag_name":TAG_NAME,
+            "error":str(e)
+        })
+    )    
 
     time.sleep(5)
